3d58ccb6026bc6cffc109974c0d7a696bcd277ba
nikhilfrancis <nikhil.francis@gmail.com>
2018-04-28 19:07:21 -0500

    New files from spring starter

---
+++ src/main/java/com/nikhil/spring/greatgif/GreatGifApplication.java
@@ -0,0 +1,12 @@
+package com.nikhil.spring.greatgif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatGifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreatGifApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nikhil/spring/greatgif/GreatGifApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nikhil.spring.greatgif;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GreatGifApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
